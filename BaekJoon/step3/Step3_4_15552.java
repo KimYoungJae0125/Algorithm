f90d38407c1dadd411b0d0de9dd2e63a12ce7748
KimYoungJae <uni0125@naver.com>
2022-07-02 11:55:42 +0900

    Step3 추가
    Step3 반복문 푸는 중

--- BaekJoon/step3/Step3_4_15552.java
+++ BaekJoon/step3/Step3_4_15552.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 //빠른 A+B
-public class Step3_1_15552 {
+public class Step3_4_15552 {
     /*
      * 본격적으로 for문 문제를 풀기 전에 주의해야 할 점이 있다. 입출력 방식이 느리면 여러 줄을 입력받거나 출력할 때 시간초과가 날 수 있다는 점이다.
      *
