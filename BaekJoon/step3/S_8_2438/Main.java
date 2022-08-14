92a28cd0f943716186b12ec0e750e9720ff529ae
KimYoungJae <uni0125@naver.com>
2022-08-14 18:57:35 +0900

    별 찍기 - 1

---
+++ BaekJoon/step3/S_8_2438/Main.java
@@ -0,0 +1,19 @@
+package BaekJoon.step3.S_8_2438;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        for(int i=0, len = Integer.parseInt(br.readLine()); i<len; i++){
+            for(int j=0, lenJ = i+1; j<lenJ; j++){
+                sb.append("*");
+            }
+                sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
