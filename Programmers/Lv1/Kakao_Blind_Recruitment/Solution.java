95ab538a829f1fec27be9cab30ae1e13ab769c3c
KimYoungJae <uni0125@naver.com>
2022-08-14 12:53:04 +0900

    2022 KAKAO BLIND RECRUITMENT 신고 결과 받기

---
+++ Programmers/Lv1/Kakao_Blind_Recruitment/Solution.java
@@ -0,0 +1,53 @@
+package Programmers.Lv1.Kakao_Blind_Recruitment;
+
+import java.util.*;
+
+public class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+//        int[] answer = {};
+        int[] answer = new int[id_list.length];
+        Map<String, Set<String>> report_map = new HashMap<>();
+        Map<String, Integer> answer_map = new HashMap<>();
+        for(String id : id_list) {
+            report_map.put(id, new HashSet<>());
+            answer_map.put(id, 0);
+        }
+        for(String report_id : report) {
+            String[] split_id = report_id.split(" ");
+            if(!split_id[0].equals(split_id[1])){
+                report_map.get(split_id[1]).add(split_id[0]);
+            }
+        }
+        for(String id : id_list){
+            if(report_map.get(id).size() >= k) {
+                for(String report_id : report_map.get(id)){
+                    answer_map.put(report_id, answer_map.get(report_id)+1);
+                }
+            }
+        }
+        for(int i=0, len = id_list.length; i<len; i++){
+            answer[i] = answer_map.get(id_list[i]);
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+        String[] report = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"};
+        int k = 2;
+        int[] answer = solution.solution(id_list, report, k);
+        for(int i : answer) {
+            System.out.println(i);
+        }
+
+        String[] id_list2 = {"con", "ryan"};
+        String[] report2 = {"ryan con", "ryan con", "ryan con", "ryan con"};
+        int k2 = 3;
+        int[] answer2 = solution.solution(id_list2, report2, k2);
+        for(int i : answer2) {
+            System.out.println(i);
+        }
+
+    }
+}
