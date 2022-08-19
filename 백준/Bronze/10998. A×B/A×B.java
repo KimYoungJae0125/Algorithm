28f42ac20512dbf9bfefd42f9f9cc5c2c3178c26
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-08-19 09:22:07 +0900

    [Bronze V] Title: A×B, Time: 244 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+				
+		int a = sc.nextInt();
+		while (a<0) {
+			System.out.print("a는 0보다 커야합니다. \n다시 입력해주세요 : ");
+			a = sc.nextInt();
+		};
+		
+		int b = sc.nextInt();
+		while(10<b) {
+			System.out.print("b는 10보다 작아야합니다.\n다시 입력해주세요 : ");
+			b = sc.nextInt();
+		}
+		
+		System.out.println(a*b);
+		
+	}
+}
