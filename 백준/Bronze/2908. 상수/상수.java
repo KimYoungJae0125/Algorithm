55d1d2ab0a65e8b44ec20a8bd43a070a918d96ba
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-08-29 14:16:47 +0900

    [Bronze II] Title: 상수, Time: 120 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -6,13 +6,10 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int a = Integer.parseInt(sb.append(st.nextToken()).reverse().toString());
-        int b = Integer.parseInt(sb2.append(st.nextToken()).reverse().toString());
+        int a = Integer.parseInt(new StringBuilder().append(st.nextToken()).reverse().toString());
+        int b = Integer.parseInt(new StringBuilder().append(st.nextToken()).reverse().toString());
         if(a>b) System.out.println(a);
         if(a<b) System.out.println(b);
-
     }
 }
