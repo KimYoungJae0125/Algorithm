da16d074affc6678f55a34b0634f4644070ab6fd
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-09-06 01:29:02 +0900

    Time: 16 ms (18.81%), Space: 46.8 MB (33.51%) - LeetHub

--- 릿코드/13-roman-to-integer/13-roman-to-integer.java
+++ 릿코드/13-roman-to-integer/13-roman-to-integer.java
@@ -1,70 +1,47 @@
 class Solution {
     public int romanToInt(String s) {
-               char[] c = s.toCharArray();
+       char[] c = s.toCharArray();
         List<Integer> list = new ArrayList<>();
         for(int i=0; i<c.length; i++) {
-            int result = 0;
-            switch (c[i]) {
-                case 'I' :
-                    if(i < c.length-1) {
-                        if(c[i+1] == 'V') {
-                            result = 4;
-                            i++;
-                        } else if(c[i+1] == 'X') {
-                            result = 9;
-                            i++;
-                        } else {
-                            result = 1;
-                        }
-                    } else {
-                        result = 1;
-                    }
-                    break;
-                case 'V' :
-                    result = 5;
-                    break;
-                case 'X' :
-                    if(i < c.length-1) {
-                        if(c[i+1] == 'L') {
-                            result = 40;
-                            i++;
-                        } else if(c[i+1] == 'C') {
-                            result = 90;
-                            i++;
-                        } else {
-                            result = 10;
-                        }
-                    } else {
-                        result = 10;
-                    }
-                    break;
-                case 'L' :
-                    result = 50;
-                    break;
-                case 'C' :
-                    if (i < c.length-1) {
-                        if(c[i+1] == 'D') {
-                            result = 400;
-                            i++;
-                        } else if(c[i+1] == 'M') {
-                            result = 900;
-                            i++;
-                        } else {
-                            result = 100;
-                        }
-                    } else {
-                        result = 100;
-                    }
-                    break;
-                case 'D' :
-                    result = 500;
-                    break;
-                case 'M' :
-                    result = 1000;
-                    break;
+            int result = charToInt(c[i]);
+            if (i < c.length - 1) {
+                boolean flag = c[i] == 'I' && (c[i + 1] == 'V' || c[i + 1] == 'X')
+                            || c[i] == 'X' && (c[i + 1] == 'L' || c[i + 1] == 'C')
+                            || c[i] == 'C' && (c[i + 1] == 'D' || c[i + 1] == 'M');
+                if(flag) {
+                    result = charToInt(c[i + 1]) - result;
+                    i++;
+                }
             }
             list.add(result);
         }
         return IntStream.range(0, list.size()).map(list::get).sum();
+    }
+    int charToInt(char c) {
+        int result = 0;
+        switch (c) {
+            case 'I' :
+                result = 1;
+                break;
+            case 'V' :
+                result = 5;
+                break;
+            case 'X' :
+                result = 10;
+                break;
+            case 'L' :
+                result = 50;
+                break;
+            case 'C' :
+                result = 100;
+                break;
+            case 'D' :
+                result = 500;
+                break;
+            case 'M' :
+                result = 1000;
+                break;
+        }
+        return result;
     }
 }
