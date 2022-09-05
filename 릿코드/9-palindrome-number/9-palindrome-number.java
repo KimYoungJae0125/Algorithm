2a5849764995bd342d872b5970752ef8bfa7d6ac
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-09-05 21:45:38 +0900

    Time: 14 ms (61.64%), Space: 46.4 MB (13.63%) - LeetHub

--- 릿코드/9-palindrome-number/9-palindrome-number.java
+++ 릿코드/9-palindrome-number/9-palindrome-number.java
@@ -1,7 +1,5 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(x).reverse();
-        return String.valueOf(x).equals(sb.toString());
+        return String.valueOf(x).equals(new StringBuilder().append(x).reverse().toString());
     }
 }
