4ba62c49c2db50cb6e6148673e57db93564c7414
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-09-06 01:59:01 +0900

    Time: 32 ms (7.05%), Space: 52.5 MB (5.09%) - LeetHub

--- 릿코드/4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 릿코드/4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,12 +1,9 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int[] list = Stream.of(nums1, nums2).flatMapToInt(Arrays::stream).sorted().toArray();
-        int listLen = list.length;
-        int index = list.length/2;
+        int listLen = list.length, index = listLen/2;
         double result = list[index];
-        if(listLen%2 ==0) {
-            result = (double)(list[index-1] + list[index])/2;
-        } 
-        return result;  
+        if(listLen%2 ==0) result = (double)(list[index-1] + list[index])/2;
+        return result;
     }
 }
