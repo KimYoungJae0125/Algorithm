69eb10ca50dfa07a788738b22d6f451274ee405a
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-09-06 23:44:05 +0900

    Time: 412 ms (5.18%), Space: 47.3 MB (58.44%) - LeetHub

--- 릿코드/23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 릿코드/23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,34 +10,22 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-          ListNode node = null;
+          ListNode node = new ListNode();
+          ListNode cur = node;
           while(true) {
-              int max = 999999999, index = -1;
+              int max = 999999999, breakInt = -1;
               for(int i=0, len= lists.length; i<len; i++) {
                   if(lists[i] == null) continue;
                   if(lists[i].val < max) {
                       max = lists[i].val;
-                      index = i;
+                      breakInt = i;
                   }
               }
-              if(index == -1) break;
-              if(node == null) {
-                  node = new ListNode(max);
-              } else {
-                  node = new ListNode(max, node);
-              }
-              lists[index] = lists[index].next;
+              if(breakInt == -1) break;
+              cur.next = lists[breakInt];
+              cur = cur.next;
+              lists[breakInt] = lists[breakInt].next;
           }
-          ListNode resultNode = null;
-          while(node != null) {
-              if(resultNode == null) {
-                  resultNode = new ListNode(node.val);
-              } else {
-                  resultNode = new ListNode(node.val, resultNode);
-              }
-              node = node.next;
-          }
-
-          return resultNode;
+          return node.next;
     }
 }
