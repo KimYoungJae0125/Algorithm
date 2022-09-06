62b561a73e17717c0a23deed41616e4c1fcd36d5
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-09-06 23:22:54 +0900

    Time: 351 ms (8.39%), Space: 47.7 MB (35.18%) - LeetHub

--- 릿코드/23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 릿코드/23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,23 +10,34 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        List<Integer> list = new ArrayList<>();
-        for(ListNode n : lists) {
-          while(n != null) {
-              list.add(n.val);
-              n = n.next;
+          ListNode node = null;
+          while(true) {
+              int max = 999999999, breakInt = -1;
+              for(int i=0, len= lists.length; i<len; i++) {
+                  if(lists[i] == null) continue;
+                  if(lists[i].val < max) {
+                      max = lists[i].val;
+                      breakInt = i;
+                  }
+              }
+              if(breakInt == -1) break;
+              if(node == null) {
+                  node = new ListNode(max);
+              } else {
+                  node = new ListNode(max, node);
+              }
+              lists[breakInt] = lists[breakInt].next;
           }
-        }
-        Integer[] a = list.stream().sorted(Comparator.reverseOrder()).toArray(Integer[]::new);
-        ListNode node = null;
-        for(int i : a) {
-            if(node == null) {
-                node = new ListNode(i);
-            } else {
-                node = new ListNode(i, node);
-            }
-        }
+          ListNode resultNode = null;
+          while(node != null) {
+              if(resultNode == null) {
+                  resultNode = new ListNode(node.val);
+              } else {
+                  resultNode = new ListNode(node.val, resultNode);
+              }
+              node = node.next;
+          }
 
-        return node;
+          return resultNode;
     }
 }
