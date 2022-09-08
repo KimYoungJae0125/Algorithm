096d2fc04e28e383fca654ac468801fd59151338
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-09-09 00:02:32 +0900

    Time: 3 ms (22.16%), Space: 41.8 MB (66.72%) - LeetHub

--- 릿코드/500-keyboard-row/500-keyboard-row.java
+++ 릿코드/500-keyboard-row/500-keyboard-row.java
@@ -3,21 +3,20 @@ class Solution {
         char[] firstC = new char[]{'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'};
         char[] secondC = new char[]{'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'};
         char[] thirdC = new char[]{'z', 'x', 'c', 'v', 'b', 'n', 'm'};
-        List<Integer> list = new ArrayList<>();
-        for(int i=0, len= words.length; i<len; i++) {
-            char[] c = words[i].toLowerCase().toCharArray();
+        return Arrays.stream(words).filter(s -> {
+            char[] c = s.toLowerCase().toCharArray();
             int count = 0;
-            for(int j=0, lenJ=firstC.length; j<lenJ; j++) {
-                for(int k=0, lenK=c.length; k<lenK; k++) {
-                    if(firstC[j] == c[k]) {
+            for (int j = 0, lenJ = firstC.length; j < lenJ; j++) {
+                for (int k = 0, lenK = c.length; k < lenK; k++) {
+                    if (firstC[j] == c[k]) {
                         count = 1;
                     }
                 }
             }
-            for(int j=0, lenJ=secondC.length; j<lenJ; j++) {
-                if(count == -1) break;
-                for(int k=0, lenK=c.length; k<lenK; k++) {
-                    if(secondC[j] == c[k]) {
+            for (int j = 0, lenJ = secondC.length; j < lenJ; j++) {
+                if (count == -1) break;
+                for (int k = 0, lenK = c.length; k < lenK; k++) {
+                    if (secondC[j] == c[k]) {
                         if (count == 1) {
                             count = -1;
                             break;
@@ -26,10 +25,10 @@ class Solution {
                     }
                 }
             }
-            for(int j=0, lenJ=thirdC.length; j<lenJ; j++) {
-                if(count == -1) break;
-                for(int k=0, lenK=c.length; k<lenK; k++) {
-                    if(thirdC[j] == c[k]) {
+            for (int j = 0, lenJ = thirdC.length; j < lenJ; j++) {
+                if (count == -1) break;
+                for (int k = 0, lenK = c.length; k < lenK; k++) {
+                    if (thirdC[j] == c[k]) {
                         if (count == 1 || count == 2 || count == -1) {
                             count = -1;
                             break;
@@ -38,8 +37,7 @@ class Solution {
                     }
                 }
             }
-            if(count != -1) list.add(i);
-        }
-        return list.stream().distinct().map(i -> words[i]).toArray(String[]::new);
+            return count != -1;
+        }).toArray(String[]::new);
     }
 }
