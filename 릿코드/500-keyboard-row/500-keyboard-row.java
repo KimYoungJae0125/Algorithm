9cd67be218fa166cfc36bb7fdfe92a9ff0691b3e
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-09-09 09:48:47 +0900

    Time: 9 ms (5.84%), Space: 42.1 MB (42.74%) - LeetHub

--- 릿코드/500-keyboard-row/500-keyboard-row.java
+++ 릿코드/500-keyboard-row/500-keyboard-row.java
@@ -1,40 +1,30 @@
 class Solution {
     public String[] findWords(String[] words) {
-        char[] firstC = new char[]{'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'};
-        char[] secondC = new char[]{'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'};
-        char[] thirdC = new char[]{'z', 'x', 'c', 'v', 'b', 'n', 'm'};
+        String firstS = "qwertyuiop";
+        String secondS = "asdfghjkl";
+        String thirdS = "zxcvbnm";
         return Arrays.stream(words).filter(s -> {
-            char[] c = s.toLowerCase().toCharArray();
+            String[] c = s.toLowerCase().split("");
             int count = 0;
-            for (int j = 0, lenJ = firstC.length; j < lenJ; j++) {
-                for (int k = 0, lenK = c.length; k < lenK; k++) {
-                    if (firstC[j] == c[k]) {
-                        count = 1;
+            for(int i =0, len = c.length; i<len; i++) {
+                if(count == -1) break;
+                if(firstS.contains(c[i])) {
+                    if(count == 2 || count == 3) {
+                        count = -1; break;
                     }
+                    count = 1;
                 }
-            }
-            for (int j = 0, lenJ = secondC.length; j < lenJ; j++) {
-                if (count == -1) break;
-                for (int k = 0, lenK = c.length; k < lenK; k++) {
-                    if (secondC[j] == c[k]) {
-                        if (count == 1) {
-                            count = -1;
-                            break;
-                        }
-                        count = 2;
+                if(secondS.contains(c[i])) {
+                    if(count == 1 || count == 3) {
+                        count = -1; break;
                     }
+                    count = 2;
                 }
-            }
-            for (int j = 0, lenJ = thirdC.length; j < lenJ; j++) {
-                if (count == -1) break;
-                for (int k = 0, lenK = c.length; k < lenK; k++) {
-                    if (thirdC[j] == c[k]) {
-                        if (count == 1 || count == 2 || count == -1) {
-                            count = -1;
-                            break;
-                        }
-                        count = 3;
+                if(thirdS.contains(c[i])) {
+                    if(count == 1 || count == 2) {
+                        count = -1; break;
                     }
+                    count = 3;
                 }
             }
             return count != -1;
