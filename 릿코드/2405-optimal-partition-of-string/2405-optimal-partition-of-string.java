d0105e6007782a9fffc9ba3c76515219c1cb710d
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-09-12 10:00:46 +0900

    Time: 51 ms (85.71%), Space: 49.5 MB (85.71%) - LeetHub

--- 릿코드/2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 릿코드/2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -1,10 +1,9 @@
 class Solution {
     public int partitionString(String s) {
-        char[] c = s.toCharArray();
         StringBuilder sb = new StringBuilder();
-        for(int i=0, len=c.length; i<len; i++) {
-            if(sb.indexOf(String.valueOf(c[i]), sb.lastIndexOf("/")) == -1) sb.append(c[i]);
-            else sb.append("/").append(c[i]);
+        for(char c : s.toCharArray()) {
+            if(sb.indexOf(String.valueOf(c), sb.lastIndexOf("/")) == -1) sb.append(c);
+            else sb.append("/").append(c);
         }
         return sb.toString().split("/").length;
     }
