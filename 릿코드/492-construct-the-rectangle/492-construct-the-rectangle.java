25ed5c57d2d890a537ebe45ca68707494e2cd254
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-09-13 15:20:11 +0900

    Time: 759 ms (5.09%), Space: 41.8 MB (34.75%) - LeetHub

--- 릿코드/492-construct-the-rectangle/492-construct-the-rectangle.java
+++ 릿코드/492-construct-the-rectangle/492-construct-the-rectangle.java
@@ -2,10 +2,8 @@ class Solution {
     public int[] constructRectangle(int area) {
         int min = 999_999_999;
         int[] result = new int[2];
-        for(int i = (int) Math.sqrt(area); i<=area; i++) {
-            if(i > min) break;
-            for(int j=1, len=area/i; j<=len; j++) {
-                if(i < j) break;
+        for(int i = (int) Math.sqrt(area); i<=min; i++) {
+            for(int j=1, len = area/i; j<=len && i>=j; j++) {
                 if(i * j == area) {
                         min = i - j;
                         result[0] = i;
