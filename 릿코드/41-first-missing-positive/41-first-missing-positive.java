1d22622b5b4317279ced60f7c6c73a69efdff340
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-09-16 00:49:14 +0900

    Time: 29 ms (19.62%), Space: 68 MB (64.07%) - LeetHub

--- 릿코드/41-first-missing-positive/41-first-missing-positive.java
+++ 릿코드/41-first-missing-positive/41-first-missing-positive.java
@@ -1,14 +1,11 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int[] a = Arrays.stream(nums).distinct().sorted().toArray();
-        if(a[0] > 1 || a[a.length-1] < 0) return 1;
-        int count = a[0];
-        for(int i=0, len=a.length; i<len; i++) {
-            if(a[i] != count) return count;
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) set.add(num);
+        int count = 1;
+        while(true) {
+            if(!set.contains(count)) return count;
             count++;
-            if(count == 0) count++;
-            if(i < len-1) if(a[i] <= 0) count = a[i+1] > 0 ? 1 : a[i+1];
         }
-        return count;
     }
 }
