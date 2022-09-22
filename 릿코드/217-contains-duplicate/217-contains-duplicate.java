6eda23db0c36a4531defacde9f9e751204c53168
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-09-22 23:02:55 +0900

    Time: 27 ms (24.74%), Space: 68.7 MB (64.87%) - LeetHub

--- 릿코드/217-contains-duplicate/217-contains-duplicate.java
+++ 릿코드/217-contains-duplicate/217-contains-duplicate.java
@@ -1,5 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        return Arrays.stream(nums).distinct().count() != nums.length;
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) {
+            if(set.contains(num)) return true;
+            set.add(num);
+        }
+        return false;
     }
 }
