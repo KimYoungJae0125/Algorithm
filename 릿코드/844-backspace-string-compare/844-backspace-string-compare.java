24ec929d8af592a4e28615a10b89cc4c5ae32b5f
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-10-01 08:58:35 +0900

    Time: 3 ms (57.39%), Space: 42.7 MB (13.47%) - LeetHub

--- 릿코드/844-backspace-string-compare/844-backspace-string-compare.java
+++ 릿코드/844-backspace-string-compare/844-backspace-string-compare.java
@@ -3,23 +3,15 @@ class Solution {
         StringBuilder sb = new StringBuilder().append(s);
         for(int i=0; i<sb.length(); i++) {
             if(sb.charAt(i) == '#') {
-                sb.deleteCharAt(i);
-                i--;
-                if(i >= 0) {
-                    sb.deleteCharAt(i);
-                    i--;
-                }
+                sb.deleteCharAt(i--);
+                if(i >= 0) sb.deleteCharAt(i--);
             }
         }
         StringBuilder sb2 = new StringBuilder().append(t);
         for(int i=0; i<sb2.length(); i++) {
             if(sb2.charAt(i) == '#') {
-                sb2.deleteCharAt(i);
-                i--;
-                if(i >= 0) {
-                    sb2.deleteCharAt(i);
-                    i--;
-                }
+                sb2.deleteCharAt(i--);
+                if(i >= 0) sb2.deleteCharAt(i--);
             }
         }
         return sb.toString().equals(sb2.toString());
