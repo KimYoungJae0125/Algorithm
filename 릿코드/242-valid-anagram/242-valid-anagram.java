ed3a496781bc1e830d5a13a9d3ff6a87fe38f6cc
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-10-06 09:25:42 +0900

    Time: 210 ms (5.00%), Space: 50.2 MB (5.59%) - LeetHub

--- 릿코드/242-valid-anagram/242-valid-anagram.java
+++ 릿코드/242-valid-anagram/242-valid-anagram.java
@@ -1,5 +1,12 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        return Arrays.stream(s.split("")).sorted().collect(Collectors.joining()).equals(Arrays.stream(t.split("")).sorted().collect(Collectors.joining()));
+        if(s.length() != t.length()) return false;
+        StringBuilder sb1 = new StringBuilder().append(s);
+        String[] sp = t.split("");
+        for(int i=0, len=sb1.length(); i<len; i++) {
+            int index = sb1.indexOf(sp[i]);
+            if(index != -1) sb1.deleteCharAt(index);
+        }
+        return sb1.length() == 0;
     }
 }
