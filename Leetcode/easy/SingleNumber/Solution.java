2ef07685f0983de84ad49f5b3cd4497d4edc05df
KimYoungJae <uni0125@naver.com>
2022-10-08 08:18:59 +0900

    Add LeetCode

---
+++ Leetcode/easy/SingleNumber/Solution.java
@@ -0,0 +1,26 @@
+package Leetcode.easy.SingleNumber;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().singleNumber(new int[]{2,2,1}));
+        System.out.println(new Solution().singleNumber(new int[]{4,1,2,1,2}));
+        System.out.println(new Solution().singleNumber(new int[]{1}));
+    }
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : nums) {
+            map.put(num, map.get(num) != null ? map.get(num) + 1 : 0);
+        }
+        int key = 0, min = 999_999_999;
+        for(int k : map.keySet()) {
+            if(map.get(k) < min) {
+                key = k;
+                min = map.get(k);
+            }
+        }
+        return key;
+    }
+}
