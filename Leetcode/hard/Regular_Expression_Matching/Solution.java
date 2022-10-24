3a786cc31d2c7fc0e0a3dd89fcb5e3888161bf3e
KimYoungJae <uni0125@naver.com>
2022-10-24 22:21:52 +0900

    문제 솔루션

--- Leetcode/hard/Regular_Expression_Matching/Solution.java
+++ Leetcode/hard/Regular_Expression_Matching/Solution.java
@@ -18,48 +18,35 @@ public class Solution {
         System.out.println(new Solution().isMatch("aaba", "ab*a*c*a"));     //false
     }
     public boolean isMatch(String s, String p) {
-        if(s.length() != p.length() && p.indexOf('*') == -1) return false;
-        var pMap = new HashMap<Character, Integer>(){{
-            for(int i=0, len=p.length(); i<len; i++) {
-                char c = p.charAt(i);
-                if(c == '*') {
-                    put(p.charAt(i-1), -1);
-                    continue;
-                }
-                if(get(c) == null || get(c) != -1) {
-                    put(c, getOrDefault(c, 0) + 1);
-                }
-            }
-        }};
+        int sLen = s.length();
+        int pLen = p.length();
 
-        var sMap = new HashMap<Character, Integer>(){{
-            for(var c : s.toCharArray()) {
-                put(c, getOrDefault(c, 0) + 1);
+        boolean[][] dp = new boolean[sLen + 1][pLen + 1];
+        dp[0][0] = true;
+        for (int j = 2; j <= pLen; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
             }
-        }};
-
-        for(var c : sMap.keySet()) {
-            if(pMap.get(c) == null && pMap.get('.') != null) continue;
-            if(pMap.get(c) != null) {
-                if(pMap.get(c) == -1) continue;
-                if(pMap.get(c) == sMap.get(c)) continue;
-                if(pMap.get('.') != null) {
-                    if(sMap.get(c) == pMap.get(c) + pMap.get('.')) continue;
-                }
-            }
-            return false;
         }
+        for (int i = 1; i <= sLen; i++) {
+            for (int j = 1; j <= pLen; j++) {
+                char sChar = s.charAt(i - 1);
+                char pChar = p.charAt(j - 1);
+                if (sChar == pChar || pChar == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pChar == '*') {
+                    // skip matching
+                    dp[i][j] = dp[i][j - 2];
 
-        for(var c : pMap.keySet()) {
-            if((c == '.' && sMap.get(c) == null) || pMap.get(c) == -1) continue;
-            if(sMap.get(c) != null) {
-                if(sMap.get(c) == pMap.get(c)) continue;
-                if(pMap.get('.') != null && (sMap.get(c) == pMap.get('.') + pMap.get(c))) continue;
+                    // matching
+                    char pPrevChar = p.charAt(j - 2);
+                    if (pPrevChar == sChar || pPrevChar == '.') {
+                        dp[i][j] |= dp[i - 1][j];
+                    }
+                }
             }
-            return false;
         }
-
-        return true;
+        return dp[sLen][pLen];
     }
 //    private boolean isMatch(String s, String p, int sIndex, int pIndex, boolean result) {
 //        if(p.length() < sIndex) {
