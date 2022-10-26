0d74d1f37ab13f4ac3180e02a27e86294ecc21f3
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-10-27 06:54:36 +0900

    Time: 12 ms (7.77%), Space: 47.3 MB (79.24%) - LeetHub

--- 릿코드/26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 릿코드/26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -1,6 +1,7 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        var removeDuplicatesNums = new TreeSet<>(new HashSet<>(){{{{for(int num : nums) add(num);}}}}).toArray(Integer[]::new);
+        var removeDuplicatesNums = new HashSet<>(){{for(int num : nums) add(num);}}.toArray(Integer[]::new);
+        Arrays.sort(removeDuplicatesNums);
         int count = removeDuplicatesNums.length;
         for(int i=0, len=nums.length; i<len; i++)
             if(i < count) nums[i] = removeDuplicatesNums[i];
