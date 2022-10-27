a10bfb93a7660d9cba6ee714046e49c5fe33c859
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-10-28 07:11:05 +0900

    Time: 93 ms (6.41%), Space: 54.6 MB (59.14%) - LeetHub

--- 릿코드/283-move-zeroes/283-move-zeroes.java
+++ 릿코드/283-move-zeroes/283-move-zeroes.java
@@ -3,9 +3,9 @@ class Solution {
         for(int i=0, len=nums.length-1; i<len; i++) {
             if(nums[i] == 0) {
                 int count = 0;
-                while(i+count < len) {
+                while(true) {
                     count++;
-                    if(nums[i+count] != 0) break;
+                    if(i+count >= len || nums[i+count] != 0) break;
                 }
                 nums[i] = nums[i+count];
                 nums[i+count] = 0;
