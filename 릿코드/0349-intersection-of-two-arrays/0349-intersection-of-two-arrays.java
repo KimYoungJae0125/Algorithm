b965a6340f6d5cea6d3686c69bae814a0629c1a7
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-11-05 01:12:31 +0900

    Time: 13 ms (11.95%), Space: 44.1 MB (44.73%) - LeetHub

--- 릿코드/0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 릿코드/0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> set = new HashSet<>(){{for(int num : nums1) add(num);}};
-        Set<Integer> set2 = new HashSet<>(){{for(int num : nums2) add(num);}};
 
         return set.stream().filter(s -> {
-            for(int num : set2) {
+            for(int num : nums2) {
                 if(s.intValue() == num) return true;
             }
             return false;
