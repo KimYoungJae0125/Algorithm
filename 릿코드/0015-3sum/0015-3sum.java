96f449e0e7c23f1a8fb88542a0eaa51eaec1c7a0
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-11-06 11:07:39 +0900

    Time: 675 ms (20.38%), Space: 46.6 MB (90.58%) - LeetHub

--- 릿코드/0015-3sum/0015-3sum.java
+++ 릿코드/0015-3sum/0015-3sum.java
@@ -2,19 +2,12 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         var result = new HashSet<List<Integer>>();
         Arrays.sort(nums);
-        for(int i=0, len=nums.length; i<len; i++) {
-        int indexOne = 0, indexTwo = len-1;
-            while(indexOne < indexTwo) {
-                if(i == indexOne) indexOne++;
-                if(i == indexTwo) indexTwo--;
+        for(int i=0, len=nums.length-2; i<len; i++) {
+        int indexOne = i+1, indexTwo = len+1;
+            while(indexOne < indexTwo) {                
                 if(indexOne == indexTwo) break;
                 int sum = nums[i] + nums[indexOne] + nums[indexTwo];
-                if(sum == 0) {
-                    int[] resultSum = new int[]{nums[i], nums[indexOne], nums[indexTwo]};
-                    Arrays.sort(resultSum);
-                    result.add(Arrays.asList(resultSum[0], resultSum[1], resultSum[2]));
-                    indexOne++;
-                }
+                if(sum == 0) result.add(Arrays.asList(nums[i], nums[indexOne++], nums[indexTwo--]));
                 if(sum < 0) indexOne++;
                 if(sum > 0) indexTwo--;
             }
