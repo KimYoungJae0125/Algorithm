fee6ca1a2de85d054527ead8647b8d724faaf376
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-11-17 09:27:53 +0900

    Time: 26 ms (54.05%), Space: 47.4 MB (43.72%) - LeetHub

--- 릿코드/0326-power-of-three/0326-power-of-three.java
+++ 릿코드/0326-power-of-three/0326-power-of-three.java
@@ -1,12 +1,5 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n < 1) return false;
-        if(n == 1) return true;
-        int a = 3;
-        for(int i=0; i<31; i++){
-            if(n-a == 0) return true;
-            a *= 3;
-        }
-
-        return false;    }
+        return (Math.log10(n)/Math.log10(3)) % 1 == 0;
+    }
 }
