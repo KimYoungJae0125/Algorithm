9bf741877e050fc702e20f05007c3a52b0bdc037
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-11-26 10:48:47 +0900

    Time: 2 ms (15.12%), Space: 41.9 MB (79.29%) - LeetHub

--- 릿코드/0075-sort-colors/0075-sort-colors.java
+++ 릿코드/0075-sort-colors/0075-sort-colors.java
@@ -1,8 +1,8 @@
 class Solution {
     public void sortColors(int[] nums) {
         for(int i=0, len=nums.length; i<len; i++) {
-            for(int j=0; j<len; j++) {
-                if(nums[i] <= nums[j]) {
+            for(int j=i; j<len; j++) {
+                if(nums[i] > nums[j]) {
                     int newNum = nums[j];
                     nums[j] = nums[i];
                     nums[i] = newNum;
