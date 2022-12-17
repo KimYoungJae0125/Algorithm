2e3ea76b81365a86ef17e27a3811e7a144112ebb
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2022-12-17 10:12:30 +0900

    Time: 10 ms (7.40%), Space: 52.1 MB (78.46%) - LeetHub

--- 릿코드/0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 릿코드/0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,11 +1,13 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        var map = new HashMap<Integer, Integer>();
         var set = new HashSet<Integer>();
         for(var num : nums) {
-            map.put(num, (map.getOrDefault(num, 0)+1));
-            set.add(num);
-            if(map.get(num) == 2) set.remove(num);
+            boolean flag = false;
+            if(!set.contains(num)) {
+                set.add(num);
+                flag = true;
+            }
+            if(!flag) set.remove(num);
         }
         return set.iterator().next();
     }
