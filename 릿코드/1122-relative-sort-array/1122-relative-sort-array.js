4fc6f0f3727daea86ff7e19167c9d0d0b623b845
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2023-02-15 16:12:54 +0900

    Time: 81 ms (29.44%), Space: 43.5 MB (32.03%) - LeetHub

--- 릿코드/1122-relative-sort-array/1122-relative-sort-array.js
+++ 릿코드/1122-relative-sort-array/1122-relative-sort-array.js
@@ -3,7 +3,6 @@
  * @param {number[]} arr2
  * @return {number[]}
  */
-const relativeSortArray = (arr1, arr2) => {
-    const arr = arr1.filter((v) => !arr2.includes(v)).sort((a, b) => a-b);
-    return [...arr2.map(v => arr1.filter(v2 => v==v2)).flat(), ...arr];
-};
+const relativeSortArray = (arr1, arr2) =>
+[...arr2.map(v => arr1.filter(v2 => v==v2)).flat()
+ , ...arr1.filter((v) => !arr2.includes(v)).sort((a, b) => a-b)];
