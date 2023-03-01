997b6d6215959511faddf86a25e55501b2150b5f
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2023-03-02 01:12:53 +0900

    Time: 14 ms (28.95%), Space: 44.7 MB (17.53%) - LeetHub

--- 릿코드/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 릿코드/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,28 +1,28 @@
 class Solution {
     public String reverseVowels(String s) {
-        Queue<int[]> queue = new PriorityQueue<>((a, b) -> b[0] - a[0] );
-        Queue<String> queue2 = new LinkedList<>();
-        String st[] = s.split("");
-        Map<String, Boolean> map = new HashMap<>(){{
-            put("A", true); put("a", true);
-            put("E", true); put("e", true);
-            put("I", true); put("i", true);
-            put("O", true); put("o", true);
-            put("U", true); put("u", true);
+        Queue<int[]> queue = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        char[] c = new char[s.length()];
+        char[] reverseC = new char[s.length()];
+        int count = 0;
+        Map<Character, Boolean> map = new HashMap<>(){{
+            put('A', true); put('a', true);
+            put('E', true); put('e', true);
+            put('I', true); put('i', true);
+            put('O', true); put('o', true);
+            put('U', true); put('u', true);
         }};
         for(int i=0, len=s.length(); i<len; i++) {
-            if(map.get(st[i]) != null) {
+            if(map.get(s.charAt(i)) != null) {
                 queue.offer(new int[]{i});
-                queue2.offer(st[i]);
+                reverseC[count++] = s.charAt(i);
             }
+            c[i] = s.charAt(i);
         }
-
-        StringBuilder sb = new StringBuilder().append(s);
         while(!queue.isEmpty()) {
             int index = queue.poll()[0];
-            sb.replace(index, index+1, queue2.poll());
+            c[index] = reverseC[--count];
         }
 
-        return sb.toString();
+        return new String(c);
     }
 }
