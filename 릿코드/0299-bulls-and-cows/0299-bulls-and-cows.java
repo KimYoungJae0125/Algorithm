c34e795890faf10ee0e96e414d150a76952fa76f
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2023-03-02 08:20:17 +0900

    Time: 7 ms (54.26%), Space: 42.8 MB (24.84%) - LeetHub

--- 릿코드/0299-bulls-and-cows/0299-bulls-and-cows.java
+++ 릿코드/0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -1,31 +1,27 @@
 class Solution {
     public String getHint(String secret, String guess) {
         int a = 0, b = 0, length = secret.length();
+        Map<Character, Integer> map = new HashMap<>();
         char[] check = new char[length];
-        char[] check2 = new char[length];
         for(int i = 0; i < length; i++) {
             if(secret.charAt(i) == guess.charAt(i)) {
                 check[i] = (char) -1;
-                check2[i] = (char) -1;
                 a++;
                 continue;
             }
-            check[i] = secret.charAt(i);
-            check2[i] = guess.charAt(i);
+            map.put(secret.charAt(i), map.getOrDefault(secret.charAt(i), 0) + 1);
         }
-
         for(int i = 0; i < length; i++) {
-            if(check[i] == (char) -1) continue;
-            for(int j = 0; j < length; j++) {
-                if(check2[j] == (char) -1) continue;
-                if(check[i] == check2[j]) {
+            if(check[i] != (char) -1) {
+                char c = guess.charAt(i);
+                Integer count = map.get(c);
+                if(count != null && count > 0) {
                     b++;
-                    check[i] = (char) -1;
-                    check2[j] = (char) -1;
+                    map.put(c, --count);
                 }
             }
         }
-
+        
         return a + "A" + b + "B";
     }
 }
