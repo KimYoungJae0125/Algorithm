4ab4640665ebaa97bab79ceb821bb4718678170f
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2023-03-02 01:01:53 +0900

    Time: 62 ms (19.70%), Space: 51.6 MB (5.04%) - LeetHub

--- 릿코드/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 릿코드/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -3,23 +3,20 @@ class Solution {
         Queue<int[]> queue = new PriorityQueue<>((a, b) -> b[0] - a[0] );
         Queue<String> queue2 = new LinkedList<>();
         String st[] = s.split("");
+        Map<String, Boolean> map = new HashMap<>(){{
+            put("A", true); put("a", true);
+            put("E", true); put("e", true);
+            put("I", true); put("i", true);
+            put("O", true); put("o", true);
+            put("U", true); put("u", true);
+        }};
         for(int i=0, len=s.length(); i<len; i++) {
-            switch (st[i]) {
-                case "A" :
-                case "a" :
-                case "E" :
-                case "e" :
-                case "I" :
-                case "i" :
-                case "O" :
-                case "o" :
-                case "U" :
-                case "u" :
-                    queue.offer(new int[]{i});
-                    queue2.offer(st[i]);
-                    break;
+            if(map.get(st[i]) != null) {
+                queue.offer(new int[]{i});
+                queue2.offer(st[i]);
             }
         }
+
         StringBuilder sb = new StringBuilder().append(s);
         while(!queue.isEmpty()) {
             int index = queue.poll()[0];
