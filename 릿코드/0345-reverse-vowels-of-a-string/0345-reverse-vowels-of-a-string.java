d880d6dba5ea93e9e130a111dcd55ae1f54a1edf
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2023-03-02 01:15:44 +0900

    Time: 12 ms (30.87%), Space: 43.5 MB (33.65%) - LeetHub

--- 릿코드/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 릿코드/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,22 +1,18 @@
 class Solution {
     public String reverseVowels(String s) {
         Queue<int[]> queue = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        char[] c = new char[s.length()];
-        char[] reverseC = new char[s.length()];
-        int count = 0;
-        Map<Character, Boolean> map = new HashMap<>(){{
-            put('A', true); put('a', true);
-            put('E', true); put('e', true);
-            put('I', true); put('i', true);
-            put('O', true); put('o', true);
-            put('U', true); put('u', true);
-        }};
-        for(int i=0, len=s.length(); i<len; i++) {
-            if(map.get(s.charAt(i)) != null) {
-                queue.offer(new int[]{i});
-                reverseC[count++] = s.charAt(i);
+        int length = s.length(), count = 0;
+        char[] c = new char[length], reverseC = new char[length];
+
+        for(int i = 0; i < length; i++) {
+            char sc = s.charAt(i);
+            switch (sc) {
+                case 'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u' -> {
+                    queue.offer(new int[]{i});
+                    reverseC[count++] = sc;
+                }
             }
-            c[i] = s.charAt(i);
+            c[i] = sc;
         }
         while(!queue.isEmpty()) {
             int index = queue.poll()[0];
