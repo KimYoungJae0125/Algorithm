19727ada18f3e39d2123c7267933ca6e6d2117a5
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2023-03-22 20:08:36 +0900

    Time: 3 ms (90.58%), Space: 51.6 MB (23.54%) - LeetHub

--- 릿코드/0200-number-of-islands/0200-number-of-islands.java
+++ 릿코드/0200-number-of-islands/0200-number-of-islands.java
@@ -1,12 +1,16 @@
 class Solution {
+    boolean[][] visited;
+    int length1, length2;
+
     public int numIslands(char[][] grid) {
-        int length1 = grid.length, length2 = grid[0].length;
-        boolean[][] visited = new boolean[length1][length2];
+        length1 = grid.length;
+        length2 = grid[0].length;
+        visited = new boolean[length1][length2];
         int result = 0;
-        for(int i = 0; i < length1; i++) {
-            for(int j = 0; j < length2; j++) {
-                if(!visited[i][j] && grid[i][j] == '1') {
-                    check(visited, grid, i, j, length1, length2);
+        for (int i = 0; i < length1; i++) {
+            for (int j = 0; j < length2; j++) {
+                if (!visited[i][j] && grid[i][j] == '1') {
+                    isIsland(grid, i, j);
                     result++;
                 }
             }
@@ -14,19 +18,19 @@ class Solution {
         return result;
     }
 
-    private void check(boolean[][] visited, char[][]grid, int i, int j, int length1, int length2) {
+    private void isIsland(char[][] grid, int i, int j) {
         visited[i][j] = true;
-        if(j > 0 && !visited[i][j-1] && grid[i][j-1] == '1') {
-            check(visited, grid, i, j-1, length1, length2);
+        if (j > 0 && !visited[i][j - 1] && grid[i][j - 1] == '1') {
+            isIsland(grid, i, j - 1);
         }
-        if(j < length2-1 && !visited[i][j+1] && grid[i][j+1] == '1') {
-            check(visited, grid, i, j+1, length1, length2);
+        if (j < length2 - 1 && !visited[i][j + 1] && grid[i][j + 1] == '1') {
+            isIsland(grid, i, j + 1);
         }
-        if(i > 0 && !visited[i-1][j] && grid[i-1][j] == '1') {
-            check(visited, grid, i-1, j, length1, length2);
+        if (i > 0 && !visited[i - 1][j] && grid[i - 1][j] == '1') {
+            isIsland(grid, i - 1, j);
         }
-        if(i < length1-1 && !visited[i+1][j] && grid[i+1][j] == '1') {
-            check(visited, grid, i+1, j, length1, length2);
+        if (i < length1 - 1 && !visited[i + 1][j] && grid[i + 1][j] == '1') {
+            isIsland(grid, i + 1, j);
         }
     }
 }
