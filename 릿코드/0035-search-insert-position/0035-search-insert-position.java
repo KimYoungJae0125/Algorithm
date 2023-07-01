754ae202be39bdb1acf33d0a4bb1ae1a7278fc52
KimYoungJae0125 <75660658+KimYoungJae0125@users.noreply.github.com>
2023-07-02 08:56:54 +0900

    Time: 0 ms (100.00%), Space: 43.4 MB (38.20%) - LeetHub

--- 릿코드/0035-search-insert-position/0035-search-insert-position.java
+++ 릿코드/0035-search-insert-position/0035-search-insert-position.java
@@ -1,17 +1,7 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int index = 0;
-        for(int i = 0, halfLength = nums.length/2 + 1; i < halfLength; i++) {
-            if(target <= nums[i]) {
-                index = i;
-                break;
-            }
-            if(nums[nums.length - i - 1] < target) {
-                index = nums.length - i;
-                break;
-            }
-            if(i == halfLength -1) index = nums.length;
-        }
+        for(; index < nums.length; index++) if(target <= nums[index]) break;
         return index;
     }
 }
